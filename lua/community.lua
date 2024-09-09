24ffb47bcfb47531ccb6378252638987b00f80be
Barry Williams <bazzawill@gmail.com>
2024-09-10 08:06:55 +0930

    update markdown

--- lua/community.lua
+++ lua/community.lua
@@ -6,13 +6,10 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.note-taking.obsidian-nvim" },
-  { import = "astrocommunity.media.image-nvim" },
   { import = "astrocommunity.motion.harpoon" },
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.note-taking.neorg" },
   { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.note-taking.obsidian-nvim" },
   -- import/override with your plugins folder
 }
